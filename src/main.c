7ffa6219b1a39689c82793e4c1464ae6b43ba100
Michael Golovanov <mike.golovanov@gmail.com>
2025-11-07 20:41:16 +0300

    Add encoder support

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
+/*
+* Interrupts handling demo.
+* 
+* Set LED connected to PC13 on/off 
+* when ecnoder button is pressed or encoder rotated.
+*
+* Encoder pins state change on falling edge is linked 
+* to interrupt handler.
+*/
 #include <stm32f1xx.h>
 
 #define BUTTON_PORT   GPIOC
 #define BUTTON_PIN    14
-#define BUTTON_EXTI   EXTI14
+#define ENCODER_PORT   GPIOC
+#define ENCODER_PIN   15
 #define LED_PORT      GPIOC
 #define LED_PIN       13
 
@@ -30,10 +40,14 @@ static void gpio_init(void)
   GPIOC->CRH &= ~(GPIO_CRH_MODE13 | GPIO_CRH_CNF13); // set push-pull by clearing GPIO_CRH_CNF13 bits
   GPIOC->CRH |= GPIO_CRH_MODE13_1; // set output mode with 2 MHz speed
 
-  // Set PC14: input with pull-up
-  GPIOC->CRH &= ~(GPIO_CRH_MODE14 | GPIO_CRH_CNF14);
-  GPIOC->CRH |= GPIO_CRH_CNF14_1; // CNF14 = 10 → input with PU/PD
-  GPIOC->ODR |= (1 << BUTTON_PIN); // Enable pull-up
+  // Set PC14 and PC15: input with pull-up
+  GPIOC->CRH &= ~(
+      GPIO_CRH_MODE14 | GPIO_CRH_CNF14
+    | GPIO_CRH_MODE15 | GPIO_CRH_CNF15
+  );
+  GPIOC->CRH |= GPIO_CRH_CNF14_1 | GPIO_CRH_CNF15_1; // CNF14/CNF15 = 10 → input with PU/PD
+  GPIOC->ODR |= (1 << BUTTON_PIN); // Enable pull-up for PC14
+  GPIOC->ODR |= (1 << ENCODER_PIN); // Enable pull-up for PC14
 }
 
 /*
@@ -74,15 +88,15 @@ static void interrupt_init(void)
   // pin 14 position 4-bits.
   // Clear the EXTI_14 port selection bits 4-bits 
   // and set 0b0010 for select PC as input.
-  AFIO->EXTICR[3] &= ~(AFIO_EXTICR4_EXTI14);
-  AFIO->EXTICR[3] |=  AFIO_EXTICR4_EXTI14_PC;
+  AFIO->EXTICR[3] &= ~(AFIO_EXTICR4_EXTI14 | AFIO_EXTICR4_EXTI15);
+  AFIO->EXTICR[3] |=  AFIO_EXTICR4_EXTI14_PC | AFIO_EXTICR4_EXTI15_PC;
   // Enable the EXTI_14 interrupt.
-  EXTI->IMR |= EXTI_IMR_MR14;
+  EXTI->IMR |= EXTI_IMR_MR14 | EXTI_IMR_MR15;
   // Setup EXTI interrupts for falling input on the button pin PC14.
   // Disable the 'rising edge' trigger (button release).
-  EXTI->RTSR &= ~(EXTI_RTSR_TR14);
+  EXTI->RTSR &= ~(EXTI_RTSR_TR14 | EXTI_RTSR_TR15);
   // Enable the 'falling edge' trigger (button press).
-  EXTI->FTSR |= EXTI_FTSR_TR14;
+  EXTI->FTSR |= EXTI_FTSR_TR14 | EXTI_FTSR_TR15;
   // Set interrupt at minimum priority.
   NVIC_SetPriority(EXTI15_10_IRQn, 0x03);
   // Enable the NVIC interrupt.
@@ -93,14 +107,13 @@ static void interrupt_init(void)
  * Main program.
  */
 int main(void) {
-  // Initialize global variables.
-  led_off();
-
   // Enable clock for the GPIOC and AFIO peripheral.
   clock_init();
 
   // Initialize PC13 as push-pull output and PC14 as pull-up input.
   gpio_init();
+  
+  led_off();
 
   // Initialize interrupts.
   interrupt_init();
@@ -116,7 +129,7 @@ int main(void) {
 /*
 * Interrupt handler for EXTI15_10_IRQn.
 *
-* Handles the button press interrupt.
+* Handles the button press or encoder rotate interrupt.
 */
 void EXTI15_10_IRQHandler(void)
 {
@@ -125,6 +138,15 @@ void EXTI15_10_IRQHandler(void)
   {
     // If it was, clear the interrupt flag.
     EXTI->PR |= EXTI_PR_PR14; // Clear by writing 1 to pin 14 position.
+    // Toggle the LED target state.
+    target_led_state = !target_led_state;
+  }
+
+  // Check that EXTI 14 is the line that triggered.
+  if(EXTI->PR & EXTI_PR_PR15) 
+  {
+    // If it was, clear the interrupt flag.
+    EXTI->PR |= EXTI_PR_PR15; // Clear by writing 1 to pin 14 position.
     // Toggle the LED target state.
     target_led_state = !target_led_state;
   }
